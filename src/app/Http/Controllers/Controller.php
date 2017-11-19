4147a51cc9b059f4ac90e201070bc88b51fee1d0
Luis Enrique Suárez <luissuarezdev@gmail.com>
2017-11-19 17:12:56 -0400

    .dev SPACE Laravel .po Editor

---
+++ src/app/Http/Controllers/Controller.php
@@ -0,0 +1,7 @@
+<?php
+    
+    namespace DOTdevSPACE\Http\Controllers;
+    
+    class Controller {
+        
+    }
---
+++ src/app/Providers/AppServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+    
+    namespace DOTdevSPACE\Providers;
+    
+    use Illuminate\Support\ServiceProvider;
+    
+    class AppServiceProvider extends
+        ServiceProvider {
+    
+        protected $namespace = 'DOTdevSPACE\Providers\Http\Controllers';
+        
+        public function boot() {
+        
+        }
+    
+        public function register() {
+        
+        }
+        
+    }
